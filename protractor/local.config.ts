6d24e871b3b6d29632946b5afec5ec7d771b455f
lopez997 <42072817+lopez997@users.noreply.github.com>
2020-07-28 08:52:49 -0500

    Css selector (#10)
    * Continuous Integration step using travis
    
    * Css Selector as a way to test UI

--- protractor/local.config.ts
+++ protractor/local.config.ts
@@ -3,7 +3,9 @@ import { reporter } from './helpers/reporter';
 
 export const config: Config = {
   framework: 'jasmine',
-  specs: ['../test/google.spec.js'],
+  specs: ['../test/**/*.spec.js'],
+  getPageTimeout: 1000,
+
   SELENIUM_PROMISE_MANAGER: false,
   onPrepare: () => {
     reporter();
