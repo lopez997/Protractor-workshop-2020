4df119b5017343d605864fc35f294262e8a8cd1e
lopez997 <42072817+lopez997@users.noreply.github.com>
2020-07-30 09:02:54 -0500

    Increasing sleep for menu-content

--- src/page/menu-content.page.ts
+++ src/page/menu-content.page.ts
@@ -1,4 +1,4 @@
-import { $, ElementFinder } from 'protractor';
+import { $, ElementFinder, browser, ExpectedConditions } from 'protractor';
 
 export class MenuContentPage {
   private tShirtMenu: ElementFinder;
@@ -8,6 +8,7 @@ export class MenuContentPage {
   }
 
   public async goToTShirtMenu(): Promise<void> {
+    await browser.wait(ExpectedConditions.elementToBeClickable(this.tShirtMenu), 3000);
     await this.tShirtMenu.click();
   }
 }
