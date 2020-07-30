db5c0069d319949540edd5883e54907f8f2998e1
lopez997 <42072817+lopez997@users.noreply.github.com>
2020-07-29 20:00:31 -0500

    Explicit wait (#14)
    * Continuous Integration step using travis
    
    * Explicit Wait Done

--- src/page/product-added-modal.page.ts
+++ src/page/product-added-modal.page.ts
@@ -1,4 +1,4 @@
-import { $, ElementFinder } from 'protractor';
+import { $, ElementFinder, browser, ExpectedConditions } from 'protractor';
 
 export class ProductAddedPage {
   private productAdded: ElementFinder;
@@ -8,6 +8,7 @@ export class ProductAddedPage {
   }
 
   public async addProduct(): Promise<void> {
+    await browser.wait(ExpectedConditions.elementToBeClickable(this.productAdded), 3000);
     await this.productAdded.click();
   }
 }
